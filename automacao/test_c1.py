345a2a04358d351a16cb7da0a7ca80a9a88d53a0
Rafael Suzano <rafaelsuzano@hotmail.com>
2020-04-02 18:05:30 -0300

    somente origem

--- automacao/test_c1.py
+++
@@ -1,8 +0,0 @@
-
-import time
-from selenium import webdriver
-
-driver = webdriver.Chrome('C://ProjetoAtech//Atech/automacao//drivers//chromedriver_win32//chromedriver.exe')  
-driver.get("https://atech-airlines-ui-staging.herokuapp.com/")
-
-print ("rafael")
