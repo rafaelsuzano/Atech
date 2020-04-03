68aa37ab7bfeb72bd77a179909686948b882bffb
Rafael Suzano <rafaelsuzano@hotmail.com>
2020-04-02 23:01:59 -0300

    relatorios e casos de teste

--- automacao/CoreTest.py
+++ automacao/CoreTest.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
-import pytest
 import unittest
 
 
@@ -17,8 +16,7 @@ import unittest
 
 
 driver =  webdriver.Chrome('C://ProjetoAtech//Atech/automacao//drivers//chromedriver_win32//chromedriver.exe') 
-chrome_options = Options()
-chrome_options.add_argument("--disable-notifications") 
+
 driver.maximize_window()
 driver.get("https://atech-airlines-ui-staging.herokuapp.com/") 
 class FP(unittest.TestCase):
@@ -49,6 +47,10 @@ class FP(unittest.TestCase):
          
           
             print("Esse cenario contempla filtro Partida/Chegada")
+            element = WebDriverWait(driver, 1000).until(
+            EC.visibility_of_element_located((By.XPATH, "/html/body/div/div[2]/div[1]/div[5]/button")))
+            time.sleep(2)
+            
             #Consulta data Partida Chegada
             driver.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/div[1]/div/div/md-datepicker/div[1]/input").send_keys("4/01/2018")
             time.sleep(2)
@@ -63,7 +65,21 @@ class FP(unittest.TestCase):
             driver.find_element(By.CSS_SELECTOR, ".btn").click()
             driver.save_screenshot('C://ProjetoAtech//Atech//automacao//evidencias//evidencia_Partida_Chegada.png')
             time.sleep(2)
-
-    
-    def test_CT_02(self):
+       
+    #@unittest.skip("Em desenvolvimento")
+    def test_CT_03(self):
+        d1 = "4/05/2018"
+        d2  = "04/04/2018"
+        driver.refresh()
+        
+        element = WebDriverWait(driver, 1000).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div/div[2]/div[1]/div[5]/button")))
+        time.sleep(2)
+        driver.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/div[1]/div/div/md-datepicker/div[1]/input").send_keys(d1)
+        
+        
+        time.sleep(2)
+        driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div[2]/div/div/md-datepicker/div[1]/input").send_keys(d2)
+        time.sleep(2)
+        driver.find_element(By.CSS_SELECTOR, ".btn").click()
+        self.assertEqual(d1<d2,"Data de chegada menor que data de Partida")
 
