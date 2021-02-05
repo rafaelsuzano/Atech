e3b1edb351e3cbba788c66030727246af6a138a8
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-02-05 15:41:12 -0300

    Update CoreTest.py

--- automacao/CoreTest.py
+++ automacao/CoreTest.py
@@ -12,8 +12,8 @@ chrome_options = Options()
 #chrome_options = Options() chrome_options.add_argument('--headless') 
 chrome_options.add_argument('--no-sandbox') 
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path='/usr/bin//chromedriver',chrome_options=chrome_options)
-
+driver = webdriver.Chrome(executable_path='/usr/bin//chromedriver')
+#,chrome_options=chrome_options
 
 driver.maximize_window()
 driver.get("https://atech-airlines-ui-staging.herokuapp.com/") 
