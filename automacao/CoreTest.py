0ebfc3fd3d08b35015f570696989e598781f2cf9
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-02-05 14:42:56 -0300

    Update CoreTest.py

--- automacao/CoreTest.py
+++ automacao/CoreTest.py
@@ -9,7 +9,6 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 
 
-option.setBinary("/usr/bin/chromedriver")
 #chrome_options = Options() chrome_options.add_argument('--headless') 
 chrome_options.add_argument('--no-sandbox') 
 chrome_options.add_argument('--disable-dev-shm-usage') 
