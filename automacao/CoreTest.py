bc806000330094d17707594ca80218b93bf059db
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-02-05 09:54:36 -0300

    Update CoreTest.py

--- automacao/CoreTest.py
+++ automacao/CoreTest.py
@@ -14,7 +14,10 @@ import unittest
 
 
 
-
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver =  webdriver.Chrome('/bin/DriverChrome/chromedriver') 
 
