8a07c5fb4ebc639e031b8a79842393eece65a537
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-02-05 15:13:04 -0300

    Update CoreTest.py

--- automacao/CoreTest.py
+++ automacao/CoreTest.py
@@ -12,7 +12,7 @@ chrome_options = Options()
 #chrome_options = Options() chrome_options.add_argument('--headless') 
 chrome_options.add_argument('--no-sandbox') 
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path="//usr//bin//chromedriver",chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path='/usr/bin//chromedriver',chrome_options=chrome_options)
 
 
 driver.maximize_window()
