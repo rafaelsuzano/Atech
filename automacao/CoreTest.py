1a073e40b733e7db6a45db425e4ea22561897507
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-02-10 08:38:28 -0300

    Update CoreTest.py

--- automacao/CoreTest.py
+++ automacao/CoreTest.py
@@ -20,12 +20,14 @@ from selenium.webdriver.support import expected_conditions as EC
 import unittest
 
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-browser = webdriver.Chrome(options=chrome_options)
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--disable-gpu')
 
-driver =  webdriver.Chrome('/usr/local/bin/chromedriver') 
+driver =  webdriver.Chrome('/usr/local/bin/chromedriver',options=chrome_options) 
 
 driver.maximize_window()
 driver.get("https://atech-airlines-ui-staging.herokuapp.com/") 
